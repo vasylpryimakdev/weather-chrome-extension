431a9b99bd4d3bda2896ed774867032f97570cdf
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-04-08 16:56:27 +0300

    refactor: reorganize component structure and add city management

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
+import { Box, Grid, InputBase, IconButton, Paper } from "@material-ui/core";
+import { Add as AddIcon } from "@material-ui/icons";
 import "./popup.css";
-import WeatherCard from "./WeatherCard";
+import WeatherCard from "../components/WeatherCard";
 
 const App: React.FC<{}> = () => {
+  const [cities, setCities] = useState<string[]>(["Toronto"]);
+
   return (
-    <div style={{ width: "400px", height: "400px" }}>
-      <WeatherCard city="Toronto" />
-    </div>
+    <Box mx="8px" my="16px">
+      <Grid container justify="space-evenly">
+        <Grid item>
+          <Paper>
+            <Box px="15px" py="5px">
+              <InputBase placeholder="Add a city name" />
+              <IconButton>
+                <AddIcon />
+              </IconButton>
+            </Box>
+          </Paper>
+        </Grid>
+      </Grid>
+      {cities.map((city, index) => (
+        <WeatherCard city={city} key={index} />
+      ))}
+      <Box height="16px" />
+    </Box>
   );
 };
 
