ead7bb0b61b8370b0d6fdf419ba03e02627fdb1d
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-04-08 17:20:03 +0300

    feat: integrate Chrome storage with city management

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,27 +1,36 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import { Box, Grid, InputBase, IconButton, Paper } from "@material-ui/core";
 import { Add as AddIcon } from "@material-ui/icons";
 import "./popup.css";
 import WeatherCard from "../components/WeatherCard";
+import { getStoredCities, setStoredCities } from "../utils/storage";
 
 const App: React.FC<{}> = () => {
   const [cities, setCities] = useState<string[]>(["Toronto"]);
   const [cityInput, setCityInput] = useState<string>("");
+
+  useEffect(() => {
+    getStoredCities().then((cities) => setCities(cities));
+  }, []);
 
   const handleCityButtonClick = () => {
     if (cityInput === "") {
       return;
     }
     const updatedCities = [...cities, cityInput];
-    setCities(updatedCities);
-    setCityInput("");
+    setStoredCities(updatedCities).then(() => {
+      setCities(updatedCities);
+      setCityInput("");
+    });
   };
 
   const handleCityDeleteButtonClick = (index: number) => {
     cities.splice(index, 1);
     const updatedCities = [...cities];
-    setCities(updatedCities);
+    setStoredCities(updatedCities).then(() => {
+      setCities(updatedCities);
+    });
   };
 
   return (
