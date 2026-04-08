e2415aa89cc673ca5a530a2c5c5c316b8e7e5715
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-04-08 18:27:03 +0300

    feat: create options page with settings UI

--- src/options/options.tsx
+++ src/options/options.tsx
@@ -1,15 +1,48 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './options.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  Grid,
+  Switch,
+  TextField,
+  Typography,
+} from "@material-ui/core";
+import "./options.css";
 
 const App: React.FC<{}> = () => {
   return (
-    <div>
-      <img src="icon.png" />
-    </div>
-  )
-}
+    <Box mx="10%" my="2%">
+      <Card>
+        <CardContent>
+          <Grid container direction="column" spacing={4}>
+            <Grid item>
+              <Typography variant="h4">Weather Extension Options</Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="body1">Home city name</Typography>
+              <TextField fullWidth placeholder="Enter a home city name" />
+            </Grid>
+            <Grid item>
+              <Typography variant="body1">
+                Auto toggle overlay on webpage load
+              </Typography>
+              <Switch color="primary" />
+            </Grid>
+            <Grid item>
+              <Button variant="contained" color="primary">
+                Save
+              </Button>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+    </Box>
+  );
+};
 
-const root = document.createElement('div')
-document.body.appendChild(root)
-ReactDOM.render(<App />, root)
+const root = document.createElement("div");
+document.body.appendChild(root);
+ReactDOM.render(<App />, root);
