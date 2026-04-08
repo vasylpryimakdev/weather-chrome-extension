12fba11233fb76820c0eae3c63cbc8175f8ad514
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-04-08 21:31:33 +0300

    feat: add context menu for quick city addition

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,10 +1,30 @@
-import { setStoredCities, setStoredOptions } from "../utils/storage";
+import {
+  getStoredCities,
+  getStoredOptions,
+  setStoredCities,
+  setStoredOptions,
+} from "../utils/storage";
+import { fetchOpenWeatherData } from "../utils/api";
 
 chrome.runtime.onInstalled.addListener(() => {
   setStoredCities([]);
   setStoredOptions({
+    hasAutoOverlay: false,
     homeCity: "",
     tempScale: "metric",
-    hasAutoOverlay: false,
+  });
+
+  chrome.contextMenus.create({
+    contexts: ["selection"],
+    title: "Add city to weather extension",
+    id: "weatherExtension",
+  });
+});
+
+chrome.contextMenus.onClicked.addListener((event) => {
+  getStoredCities().then((cities) => {
+    if (cities) {
+      setStoredCities([...cities, event.selectionText]);
+    }
   });
 });
