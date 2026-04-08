b96f45c4c6ca7dd56a50082d4ee66644e8f3e217
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-04-08 16:38:38 +0300

    feat: enhance WeatherCard UI with Material-UI components

--- src/popup/WeatherCard/WeatherCard.tsx
+++ src/popup/WeatherCard/WeatherCard.tsx
@@ -1,16 +1,50 @@
 import React, { useEffect, useState } from "react";
-import { fetchOpenWeatherData } from "../../utils/api";
+import { Box, Grid, Typography } from "@material-ui/core";
+import { fetchOpenWeatherData, OpenWeatherData } from "../../utils/api";
 
 const WeatherCard: React.FC<{
   city: string;
 }> = ({ city }) => {
+  const [weatherData, setWeatherData] = useState<OpenWeatherData | null>(null);
+
   useEffect(() => {
     fetchOpenWeatherData(city)
-      .then((data) => console.log(data))
+      .then((data) => {
+        setWeatherData(data);
+      })
       .catch((err) => console.log(err));
   }, [city]);
 
-  return <div>{city}</div>;
+  if (!weatherData) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <Box>
+      <Grid container justify="space-around">
+        <Grid item>
+          <Typography className="weatherCard-title">
+            {weatherData.name}
+          </Typography>
+          <Typography className="weatherCard-temp">
+            {Math.round(weatherData.main.temp)}
+          </Typography>
+          <Typography className="weatherCard-body">
+            Feels like {Math.round(weatherData.main.feels_like)}
+          </Typography>
+        </Grid>
+        <Grid item>
+          {weatherData.weather.length > 0 && (
+            <>
+              <Typography className="weatherCard-body">
+                {weatherData.weather[0].main}
+              </Typography>
+            </>
+          )}
+        </Grid>
+      </Grid>
+    </Box>
+  );
 };
 
 export default WeatherCard;
